551edb277fbd06aa8f97fd4f11e57020f9fb39e8
JuliusMatadamas <julius3877@gmail.com>
2023-11-05 16:15:31 -0600

    Se crea el modelo 'Gender', se crea la conexion a la base de datos SQL Server y se realiza la migracion para crear la tabla 'Genders'.

---
+++ BulkyWeb/Migrations/20231105192148_AddGenderTableToDb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenderTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.id);
+                    table.UniqueConstraint("Unique_Name_Constraint", x => x.name);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genders");
+        }
+    }
+}
---
+++ BulkyWeb/Models/Gender.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Gender
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "El campo 'name' solo puede contener letras.")]
+        [Column("name")]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+
+        [Column("created_at")]
+        public DateTime? CreatedAt { get; set; }
+
+        [Column("deleted_at")]
+        public DateTime? DeletedAt { get; set; }
+
+        [Column("updated_at")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
